2ffc551cf28ca4fc2a800e51edee48f0c524c4fd
MertvyeDushi <RomanTyurin@outlook.com>
2021-09-01 21:40:31 +0300

    Update lec 1 ex 1

--- 2.0/lect_1/ex_1.js
+++ 2.0/lect_1/ex_1.js
@@ -1,20 +1,20 @@
 const STR = 'ababaloremtextinputercomputed     j'
 
-let map = {}
+let set = {}
 let answer = 0
 
 for (let i = 0; i <= STR.length - 1; i++) {
 	const current = STR[i]
 	
-	if (map[current]) {
-		map[current]++
+	if (set[current]) {
+		set[current]++
 
-		if (answer < map[current]) answer = map[current]
+		if (answer < set[current]) answer = set[current]
 
 	} else {
-		map[current] = 1
+		set[current] = 1
 	}
 }
 
-console.log(map)
+console.log(set)
 console.log(answer)
