8bd17e1ffe543b774561a8917c13b86a39120c28
MertvyeDushi <RomanTyurin@outlook.com>
2021-09-01 22:16:06 +0300

    Add special case handling

--- 2.0/lect_1/ex_1.js
+++ 2.0/lect_1/ex_1.js
@@ -1,19 +1,23 @@
 const STR = 'ababaloremtextinputercomputed     j'
 
+/**
+ * Особые случаи:
+ */
+
+// const STR = ''
+// const STR = 'd'
+
 let set = {}
 let answer = 0
 
 for (let i = 0; i <= STR.length - 1; i++) {
 	const current = STR[i]
 	
-	if (set[current]) {
-		set[current]++
+	if (!set[current]) set[current] = 0
 
-		if (answer < set[current]) answer = set[current]
-
-	} else {
-		set[current] = 1
-	}
+	set[current]++
+	
+	if (answer < set[current]) answer = set[current]
 }
 
 console.log(set)
